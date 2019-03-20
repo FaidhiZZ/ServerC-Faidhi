d21c6e7993fd7eb063514eccd1f4911207933918
FaidhiZZ <faidhi2j@gmail.com>
2019-03-20 21:38:03 +0800

    C final changes

--- server.c
+++ server.c
@@ -8,8 +8,7 @@
 int main()
 {
 	char buffer[1024] = {0};
-	char *reply = "Salut";
-	int valread;	
+	char *reply = "Salut";	
 	
 	// This will create the server
 	int server_socket = socket(AF_INET, SOCK_STREAM, 0);	
@@ -17,7 +16,7 @@ int main()
 	// The definition of the server address
 	struct sockaddr_in server_address;
 	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(9004);
+	server_address.sin_port = htons(666);
 	server_address.sin_addr.s_addr = INADDR_ANY;
 	
 	int addrlen = sizeof(server_address);
@@ -31,13 +30,16 @@ sizeof(server_address));
 int client_socket = accept(server_socket, (struct sockaddr*)&server_address, 
 (socklen_t*)&addrlen);
 
-	valread = read(client_socket, buffer, 1024);
+	int valread = read(client_socket, buffer, 1024);
 	
 	printf("%s\n",buffer);
 
 	// Sends the message to client
 	send(client_socket, reply, strlen(reply),0);
-
+	
+	// Closes the socket
+	close(server_socket);
+	
 	return 0;
 
 }
