b52b4e894cec0259b3b04ed1475506a537db6359
FaidhiZZ <faidhi2j@gmail.com>
2019-03-20 19:55:28 +0800

    C coding update

--- server.c
+++ server.c
@@ -1,35 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <unistd.h>
 
 int main()
 {
-	char server_message[256] = "salut du serveur";
+	char buffer[1024] = {0};
+	char *reply = "Salut";
+	int valread;	
 	
 	// This will create the server
-	int server_socket = socket(AF_INET, SOCK_STREAM, 0)	
+	int server_socket = socket(AF_INET, SOCK_STREAM, 0);	
 	
 	// The definition of the server address
 	struct sockaddr_in server_address;
 	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(9002);
+	server_address.sin_port = htons(9004);
 	server_address.sin_addr.s_addr = INADDR_ANY;
 	
+	int addrlen = sizeof(server_address);
+
 	// Binding socket
 	bind(server_socket, (struct sockaddr*) &server_address, 
 sizeof(server_address));
 
 	listen(server_socket, 3);
 
-	int client_socket = accept(server_socket, NULL, NULL);
-	// Sends the message to client
-	send(client_socket, server_message, sizeof(server_message), 0);
+int client_socket = accept(server_socket, (struct sockaddr*)&server_address, 
+(socklen_t*)&addrlen);
+
+	valread = read(client_socket, buffer, 1024);
 	
-	// Closes the socket
-	close(server_socket);	
+	printf("%s\n",buffer);
+
+	// Sends the message to client
+	send(client_socket, reply, strlen(reply),0);
 
 	return 0;
 
